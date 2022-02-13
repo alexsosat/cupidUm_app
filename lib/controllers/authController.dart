fc6ab2330e29ab635ddfc21af23e01e38107d989
celinediaz <38956985+celinediaz@users.noreply.github.com>
2022-02-13 14:33:01 -0600

    Create authController.dart

---
+++ lib/controllers/authController.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/state_manager.dart';
+import 'package:get/route_manager.dart';
+
+class AuthController extends GetxController {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //final Future<FirebaseApp> firebaseInitialization = Firebase.initializeApp();
+  late Rx<User?> _firebaseUser;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _firebaseUser = Rx<User?>(_auth.currentUser);
+    _firebaseUser.bindStream(_auth.userChanges());
+  }
+
+  void register(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      Get.snackbar(
+        "Error al crear cuenta",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+}
