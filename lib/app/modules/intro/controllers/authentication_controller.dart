ae1fa54bfbb73d59ccee4bac22201b7ae4e65a49
alexsosat <1080810@alumno.um.edu.mx>
2022-02-13 19:45:57 -0600

    Combine auth controllers

--- lib/app/modules/intro/controllers/authentication_controller.dart
+++ lib/app/modules/intro/controllers/authentication_controller.dart
@@ -4,8 +4,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthenticationController extends GetxController {
   User? _firebaseUser;
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late Rx<User?> _dynamicFirebaseUser;
+
   bool get isUserLoggedIn {
-    _firebaseUser = FirebaseAuth.instance.currentUser;
+    _firebaseUser = _auth.currentUser;
     return _firebaseUser != null;
   }
 
@@ -19,7 +22,51 @@ class AuthenticationController extends GetxController {
     super.onInit();
   }
 
+  @override
+  void onReady() {
+    super.onReady();
+    _dynamicFirebaseUser = Rx<User?>(_auth.currentUser);
+    _dynamicFirebaseUser.bindStream(_auth.userChanges());
+  }
+
   void logUserInApp() {
-    _firebaseUser = FirebaseAuth.instance.currentUser;
+    _firebaseUser = _auth.currentUser;
+  }
+
+  void register(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } catch (e) {
+      Get.snackbar(
+        "Error al crear cuenta",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
+
+  void signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      Get.snackbar(
+        "Error",
+        e.toString(),
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
   }
 }
