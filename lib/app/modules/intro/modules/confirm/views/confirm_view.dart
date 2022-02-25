56a014337f74889ae2189fac173d5ded2919a4d1
celinediaz <38956985+celinediaz@users.noreply.github.com>
2022-02-24 18:55:54 -0600

    confirm view

--- lib/app/modules/intro/modules/confirm/views/confirm_view.dart
+++ lib/app/modules/intro/modules/confirm/views/confirm_view.dart
@@ -1,21 +1,26 @@
+import 'package:cupidum_app/app/modules/intro/modules/confirm/views/confirm_header_text.dart';
+import 'package:cupidum_app/globals/pill_button.dart';
+import 'package:cupidum_app/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class ConfirmView extends StatelessWidget {
-
   ConfirmView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        'Confirm your email',
-        style: TextStyle(
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
-        )
-      )
-    );
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ConfirmHeader(),
+          PillButton(child: Text("REGRESAR"), onPressed: () => Get.back()),
+        ],
+      ),
+    ));
   }
 }
